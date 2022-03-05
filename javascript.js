9d56d1a6be273fa47a9186429eaf049319f8c15a
ElfeddinMehdizade <elfeddin991@list.ru>
2022-03-05 20:49:06 +0400

    proyekt tamamlandi

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const menimdivim = document.getElementById("qutu")
+const menimbuttonum = document.querySelector("button")
+
+
+
+menimbuttonum.onclick = yaziniyazdir
+function yaziniyazdir(){
+    menimdivim.innerText = "js derslerine xosh gelmisik!"
+}
